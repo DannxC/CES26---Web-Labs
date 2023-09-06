787738ebf18554535cab4c8b244356f5ddc3f1d5
Daniel Cavassani <101767386+DannxC@users.noreply.github.com>
2023-09-05 21:00:15 -0300

    Projeto basico feito. Funcionalidades principais feitas, bem como a adicao de um 'noh root' no container para servir como ancora final, caso todos os elementos sejam removidos.

--- Lista 3/main.js
+++ Lista 3/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
     // Seleciona o elemento clicado
-    $("body").on("click", "*", function (e) {
+    $("#container").on("click", "*", function (e) {
         e.stopPropagation();
-        if (!$(this).closest("#control-panel").length) { // Verifica se o elemento clicado não está dentro do painel de controle
-            $(".selected").removeClass("selected"); // Remove a classe de qualquer elemento previamente selecionado
-            $(this).addClass("selected"); // Adiciona a classe ao elemento clicado atualmente
+        $(".selected").removeClass("selected"); // Remove a classe de qualquer elemento previamente selecionado
+        $(this).addClass("selected"); // Adiciona a classe ao elemento clicado atualmente
+    });
+
+    // Adiciona um evento de clique ao "root"
+    $("#root").click(function (e) {
+        if (e.target === this) {  // Verifica se o clique foi diretamente no "root"
+            e.stopPropagation();  // Impede que o evento se propague para o #container
+            $(".selected").removeClass("selected");
+            $(this).addClass("selected");
         }
     });
 
     // Remove o elemento selecionado atualmente ao clicar no botão
     $("#remove-element").click(function () {
-        if (!$(".selected").is("#control-panel")) {
+        if (!$(".selected").is("#root")) {  // Verifica se o elemento selecionado não é o root
             $(".selected").remove();
         }
     });
@@ -21,5 +28,16 @@ $(document).ready(function () {
         const newElement = $(`<${elementType}>Novo Elemento</${elementType}>`);
         $(".selected").append(newElement);
     });
-});
 
+    // Edita o texto do elemento selecionado atualmente
+    $("#edit-text").on("input", function () {
+        const newText = $(this).val();
+        $(".selected").text(newText);
+    });
+
+    // Edita a cor de fundo do elemento selecionado atualmente
+    $("#edit-bg-color").on("input", function () {
+        const newColor = $(this).val();
+        $(".selected").css("background-color", newColor);
+    });
+});
